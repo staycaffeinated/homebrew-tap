8ef419b87aeeaade359980fcbdff85033dc1eb9b
Jon Caulfield <danesub@yahoo.com>
2022-07-15 08:05:35 -0500

    publish: release 3.0.0

--- Formula/metacode.rb
+++ Formula/metacode.rb
@@ -3,8 +3,8 @@
 class Metacode < Formula
   desc "A code generator for Spring-based applications"
   homepage "https://github.com/staycaffeinated/metacode/"
-  url "https://raw.githubusercontent.com/staycaffeinated/homebrew-tap/main/metacode-2.0.0.tar.gz"
-  sha256 "933f5be8b22f7ed75c507e1a6dff7444f59c2b325880ea5002e1d761a07e77be"
+  url "https://raw.githubusercontent.com/staycaffeinated/homebrew-tap/main/metacode-3.0.0.tar.gz"
+  sha256 "6d4beb5d5c2874cc9cd7f28478980c17e807a5a4cbc2eceb87ab4cd15b2aedb0"
   license "Apache 2.0"
   
   # metacode works with jdk11 or higher, but does not require java11
@@ -27,6 +27,6 @@ class Metacode < Formula
     # You can manually run the test with the command: `brew test metacode`. 
     #
     output = shell_output("#{bin}/metacode --version")
-    assert_match "2.0.0", output
+    assert_match "3.0.0", output
   end
 end
